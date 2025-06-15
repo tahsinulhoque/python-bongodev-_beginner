3afc1d9005c7fce9e16502a04ccb44ba9b2f44ca
Tahsinul Hoque siddiki <tahsinulhoque10@gmail.com>
2025-06-15 19:51:54 +0600

    problem_5

---
+++ Intermediate/solution_5.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+# Test
+print(fibonacci(50))  # Output: 12586269025
